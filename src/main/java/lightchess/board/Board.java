eed64f8ccc4d08f100e7302f51fb9d74e2679d14
jojo <yoyosource@nidido.de>
2020-02-15 22:52:32 +0100

    Add Board

--- src/main/java/lightchess/board/Board.java
+++ src/main/java/lightchess/board/Board.java
@@ -1,15 +1,61 @@
 package lightchess.board;
 
+import lightchess.board.implemented.*;
+
 public class Board {
 
     private Piece[][] pieces = new Piece[8][8];
 
-    private Board() {
+    private String defaultBoard = "WR|WN|WB|WQ|WK|WB|WN|WR|WP|WP|WP|WP|WP|WP|WP|WP||||||||||||||||||||||||||||||||BP|BP|BP|BP|BP|BP|BP|BP|BR|BN|BB|BQ|BK|BB|BN|BR";
 
+    private Board() {
+        String[] strings = defaultBoard.split("\\|");
+        if (strings.length != 64) {
+            throw new IllegalArgumentException("Board is not 64 tiles big. (" + strings.length + ")");
+        }
+        for (int i = 0; i < strings.length; i++) {
+            int x = i % 8;
+            int y = (i - x) / 8;
+            pieces[x][y] = getPiece(strings[i]);
+        }
     }
 
     private Board(String s) {
 
+    }
+
+    private Piece getPiece(String s) {
+        if (s.isEmpty()) {
+            return null;
+        }
+        if (s.length() != 2) {
+            return null;
+        }
+        PieceColor color;
+        if (s.charAt(0) == 'W') {
+            color = PieceColor.WHITE;
+        } else if (s.charAt(0) == 'B') {
+            color = PieceColor.BLACK;
+        } else {
+            return null;
+        }
+        char c = s.charAt(1);
+        if (c == 'R') {
+            return new Rook(color);
+        }
+        if (c == 'N') {
+            return new Knight(color);
+        }
+        if (c == 'B') {
+            return new Bishop(color);
+        }
+        if (c == 'Q') {
+            return new Queen(color);
+        }
+        if (c == 'K') {
+            return new King(color);
+        }
+        return null;
     }
 
     public Board createBoard() {
