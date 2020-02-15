b0e9937b17a283cb6d4c7c700c48851123e75c07
jojo <yoyosource@nidido.de>
2020-02-15 22:15:17 +0100

    Add main()

---
+++ src/main/java/lightchess/Draw.java
@@ -0,0 +1,13 @@
+package lightchess;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Draw extends JComponent {
+
+    @Override
+    protected void paintComponent(Graphics g) {
+
+    }
+
+}
--- src/main/java/lightchess/LightChess.java
+++ src/main/java/lightchess/LightChess.java
@@ -1,9 +1,38 @@
 package lightchess;
 
+import javax.swing.*;
+
 public class LightChess {
 
-    public static void main(String[] args) {
+    private static Draw draw = new Draw();
 
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame();
+        jFrame.setTitle("LightChess");
+        jFrame.setName("LightChess");
+        jFrame.setResizable(false);
+        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        jFrame.setSize(800, 600);
+        jFrame.setLocationRelativeTo(null);
+
+        jFrame.setVisible(true);
+        jFrame.setContentPane(draw);
+        jFrame.validate();
+
+        Runnable runnable = () -> {
+            while (true) {
+                jFrame.repaint();
+                try {
+                    Thread.sleep(1000/60);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();;
+                }
+            }
+        };
+        Thread t = new Thread(runnable);
+        t.setName("Renderer");
+        t.start();
     }
 
 }
