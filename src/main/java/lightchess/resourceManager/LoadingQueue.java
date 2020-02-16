a58af0f00548e5de241b5a5a307b7c07f99a5500
jojo <yoyosource@nidido.de>
2020-02-16 12:34:25 +0100

    Add ResourceManager

--- src/main/java/lightchess/resourceManager/LoadingQueue.java
+++ src/main/java/lightchess/resourceManager/LoadingQueue.java
@@ -7,28 +7,11 @@ public class LoadingQueue {
 
     private List<QueueEntry> queueEntries = new ArrayList<>();
 
-    /**
-     *
-     * @since Version 1.1
-     *
-     * @param path
-     * @param name
-     * @return
-     */
     public LoadingQueue add(String path, String name) {
         queueEntries.add(new QueueEntry(name, path));
         return this;
     }
 
-    /**
-     *
-     * @since Version 1.1
-     *
-     * @param path
-     * @param name
-     * @param async
-     * @return
-     */
     public LoadingQueue add(String path, String name, boolean async) {
         queueEntries.add(new QueueEntry(name, path, async));
         return this;
--- src/main/java/lightchess/resourceManager/QueueEntry.java
+++ src/main/java/lightchess/resourceManager/QueueEntry.java
@@ -6,26 +6,11 @@ public class QueueEntry {
     private String path;
     private boolean async = false;
 
-    /**
-     *
-     * @since Version 1.1
-     *
-     * @param name
-     * @param path
-     */
     public QueueEntry(String name, String path) {
         this.name = name;
         this.path = path;
     }
 
-    /**
-     *
-     * @since Version 1.1
-     *
-     * @param name
-     * @param path
-     * @param async
-     */
     public QueueEntry(String name, String path, boolean async) {
         this.name = name;
         this.path = path;
