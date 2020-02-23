7c6af5eb64d6d3e8463aa4e58a8accd6f349d4c2
BuildTools <unconfigured@null.spigotmc.org>
2020-02-23 23:51:49 +0100

    Main Menu

--- src/main/java/lightchess/stages/ConnectStage.java
+++ src/main/java/lightchess/stages/ConnectStage.java
@@ -25,6 +25,7 @@ public class ConnectStage implements Stage {
     private boolean[] wasselected = {false, false, false};
     Server s = null;
     Client c = null;
+    private int currentpoints = 0;
 
     @Override
     public void render(Graphics2D g) {
@@ -59,14 +60,23 @@ public class ConnectStage implements Stage {
         }
         if(s != null){
             try {
+                g.setColor(Color.GRAY);
+                g.fillRect(0,0,1400,1100);
                 InetAddress add = InetAddress.getLocalHost();
                 Fonts.string(g, new Font("Old English Text MT", Font.PLAIN, 100), Color.BLACK, add.getHostAddress(), 600);
+                String str = "";
+                for (int i = 0; i < currentpoints; i += 60) {
+                    if(i > 1){
+                        str += ".";
+                    }
+                }
+                Fonts.string(g, new Font("Old English Text MT", Font.PLAIN, 100), Color.BLACK, "Waiting for Client" + str, 287, 400);
                 Thread t = new Thread(new Runnable() {
                     @Override
                     public void run() {
                         s.waitForClient();
                     }
-                });
+                }, "Network-Waiter");
             } catch (UnknownHostException e) {
                 e.printStackTrace();
             }
@@ -77,66 +87,73 @@ public class ConnectStage implements Stage {
 
     @Override
     public void tick() {
-        if(KeyInput.wasPressed(KeyEvent.VK_ESCAPE)){
-            LightChess.draw.setStage(0);
-        }
-        boolean intersects = false;
-        for (int i = 0; i < buttons.length; i++) {
-            if (CheckIntersection.intersects(buttons[i], MouseInput.getX(), MouseInput.getY())) {
-                currentSelection = i;
-                intersects = true;
-            }
-
-        }
-
-        if (!intersects) {
-            currentSelection = -1;
-        }
-
-
-        if (MouseInput.wasPressed(1)) {
-            if (currentSelection == 0) {
+        if(s == null){
+            if(KeyInput.wasPressed(KeyEvent.VK_ESCAPE)){
                 LightChess.draw.setStage(0);
             }
-            if(currentSelection == 2){
-                s = new Server(4999, texts[2].getText());
+            boolean intersects = false;
+            for (int i = 0; i < buttons.length; i++) {
+                if (CheckIntersection.intersects(buttons[i], MouseInput.getX(), MouseInput.getY())) {
+                    currentSelection = i;
+                    intersects = true;
+                }
+
             }
-            if(currentSelection == 1){
-                c = new Client();
-                c.connect(texts[1].getText(), 4999);
+
+            if (!intersects) {
+                currentSelection = -1;
             }
-            for (int i = 0; i < texts.length; i++) {
-                if (CheckIntersection.intersects(texts[i], MouseInput.getX(), MouseInput.getY())) {
-                    textfield = i;
-                    if(!wasselected[i]){
-                        texts[i].setText("");
-                        wasselected[i] = true;
+
+
+            if (MouseInput.wasPressed(1)) {
+                if (currentSelection == 0) {
+                    LightChess.draw.setStage(0);
+                }
+                if(currentSelection == 2){
+                    s = new Server(4999, texts[2].getText());
+                }
+                if(currentSelection == 1){
+                    c = new Client();
+                    c.connect(texts[1].getText(), 4999);
+                }
+                for (int i = 0; i < texts.length; i++) {
+                    if (CheckIntersection.intersects(texts[i], MouseInput.getX(), MouseInput.getY())) {
+                        textfield = i;
+                        if(!wasselected[i]){
+                            texts[i].setText("");
+                            wasselected[i] = true;
+                        }
                     }
-                }
 
+                }
             }
-        }
-        for (int i = 0; i < 256; i++) {
-            if(KeyInput.wasPressed(i)){
-                if(i == KeyEvent.VK_BACK_SPACE){
-                    if(texts[textfield].getText().length() == 0) continue;
-                    texts[textfield].setText(texts[textfield].getText().substring(0, texts[textfield].getText().length() -1));
-                    continue;
-                }
-                if(i == KeyEvent.VK_SHIFT) continue;
-                if(i == KeyEvent.VK_ENTER) continue;
-                if(i == KeyEvent.VK_SPACE) {
-                    texts[textfield].setText(texts[textfield].getText() + " ");
-                    continue;
-                }
-                if(KeyInput.isKeyDown(KeyEvent.VK_SHIFT)){
-                    texts[textfield].setText(texts[textfield].getText() + ((char)i + "").toUpperCase());
-                }else{
-                    texts[textfield].setText(texts[textfield].getText() + ((char)i + "").toLowerCase());
-                }
+            for (int i = 0; i < 256; i++) {
+                if(KeyInput.wasPressed(i)){
+                    if(i == KeyEvent.VK_BACK_SPACE){
+                        if(texts[textfield].getText().length() == 0) continue;
+                        texts[textfield].setText(texts[textfield].getText().substring(0, texts[textfield].getText().length() -1));
+                        continue;
+                    }
+                    if(i == KeyEvent.VK_SHIFT) continue;
+                    if(i == KeyEvent.VK_ENTER) continue;
+                    if(i == KeyEvent.VK_SPACE) {
+                        texts[textfield].setText(texts[textfield].getText() + " ");
+                        continue;
+                    }
+                    if(KeyInput.isKeyDown(KeyEvent.VK_SHIFT)){
+                        texts[textfield].setText(texts[textfield].getText() + ((char)i + "").toUpperCase());
+                    }else{
+                        texts[textfield].setText(texts[textfield].getText() + ((char)i + "").toLowerCase());
+                    }
 
+                }
             }
+        }else{
+            currentSelection = -1;
+            currentpoints++;
+            if(currentpoints == 240) currentpoints = 0;
         }
+
     }
 
     @Override
--- src/main/java/lightchess/stages/OnlineStage.java
+++ src/main/java/lightchess/stages/OnlineStage.java
@@ -1,5 +1,6 @@
 package lightchess.stages;
 
+import lightchess.render.BordRenderer;
 import lightchess.render.Stage;
 
 import java.awt.*;
@@ -7,7 +8,10 @@ import java.awt.*;
 public class OnlineStage implements Stage {
     @Override
     public void render(Graphics2D g) {
+        g.setColor(Color.GRAY);
 
+        g.fillRect(0,0,1400,1100);
+        BordRenderer.renderBoard(g, 350, 30);
     }
 
     @Override
